2d90c69a3f206686c6b01de1469f74c37ab4cb66
ConnorMMM <connorchurchmill@gmail.com>
2023-04-18 16:22:53 +1000

    Created a script that can produce noise for random number generation.

---
+++ URP_Unity/Assets/Scripts/NoiseFunctions.cs
@@ -0,0 +1,69 @@
+using System.Threading.Tasks;
+
+using Unity.Mathematics;
+
+using UnityEngine;
+
+using Random = System.Random;
+
+public static class NoiseFunctions
+{
+    public static float[,] GenerateNoiseMap(int _density, NoiseSettings _settings)
+    {
+        float[,] noiseMap = new float[_density, _density];
+
+        Random prng = new(_settings.Seed);
+        float2[] octaveOffsets = new float2[_settings.octaves];
+
+        float amplitude = 1;
+        float frequency = 1;
+
+        for(int i = 0; i < _settings.octaves; i++)
+        {
+            octaveOffsets[i] = new float2()
+            {
+                x = prng.Next(-10000, 10000) + _settings.offset.x,
+                y = prng.Next(-10000, 10000) + _settings.offset.y
+            };
+        }
+
+        float halfDensity = _density / 2;
+
+        for(int x = 0; x < _density; x++)
+        {
+            for(int y = 0; y < _density; y++)
+            {
+                amplitude = 1;
+                frequency = 1;
+
+                float noiseHeight = 0;
+                
+                for(int i = 0; i < _settings.octaves; i++)
+                {
+                    float sampleX = (x - halfDensity + octaveOffsets[i].x) / _settings.scale * frequency;
+                    float sampleY = (y - halfDensity + octaveOffsets[i].y) / _settings.scale * frequency;
+
+                    float simplex = Remap(noise.srnoise(new float2(sampleX, sampleY)), -1, 1, 0, 1);
+                    
+                    float perlin = Remap(noise.cnoise(new float2(sampleX, sampleY)), -1, 1, 0, 1);
+
+                    float t = _settings.interpolationCurve.Evaluate(i / _settings.octaves);
+                    
+                    noiseHeight += (Mathf.Lerp(simplex, perlin, t)) * amplitude;
+
+                    amplitude *= _settings.persistance;
+                    frequency *= _settings.lacunarity;
+                }
+
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        return noiseMap;
+    }
+    
+    public static float Remap(float _val, float _oldMin, float _oldMax, float _newMin, float _newMax)
+    {
+        return (_val - _oldMin) / (_oldMax - _oldMin) * (_newMax - _newMin) + _newMin;
+    }
+}
---
+++ URP_Unity/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class TerrainGenerator : MonoBehaviour
+{
+	[SerializeField] private int density = 128;
+	[SerializeField] private float spacing = 0.5f;
+	[SerializeField] private NoiseSettings noiseSettings;
+	[SerializeField] private bool alwaysRegen;
+
+	private MeshFilter filter;
+	private new MeshRenderer renderer;
+
+	private void Start()
+	{
+		filter = gameObject.GetComponent<MeshFilter>();
+		renderer = gameObject.GetComponent<MeshRenderer>();
+
+		float[,] noiseMap = NoiseFunctions.GenerateNoiseMap(density, noiseSettings);
+
+		filter.mesh = MeshGenerator.Generate(density, noiseMap, spacing);
+	}
+
+	private void Update()
+	{
+		if(alwaysRegen)
+		{
+			float[,] noiseMap = NoiseFunctions.GenerateNoiseMap(density, noiseSettings);
+
+			filter.mesh = MeshGenerator.Generate(density, noiseMap, spacing);
+		}
+	}
+}
